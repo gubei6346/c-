b00a2b1fdc806174cc6a1166c10213b6e250aadd
gubei6346 <gubeiiii6346@gmail.com>
2024-01-17 18:02:46 +0800

    test

---
+++ learn-c/learn-c/test01.c
@@ -0,0 +1,24 @@
+//分支语句
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main() {
+	int age = 20;
+	/*if (age >= 18)
+		printf("成年\n");
+	else {
+		printf("未成年\n");
+		printf("不能玩原神");
+	}*/
+
+	/*if (age < 18)
+		printf("少年\n");
+	else if (age >= 18 && age < 26)
+		printf("青年");
+	else if (age >= 26 && age < 40)
+		printf("中年");
+	else if (age >= 40 && age < 60)
+		printf("壮年");*/
+
+	//if-else就近匹配原则
+
+}
