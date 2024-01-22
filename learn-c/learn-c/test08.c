9af090d5a11f1932412a7df7eab61b8b23121a32
gubei6346 <gubeiiii6346@gmail.com>
2024-01-22 15:25:00 +0800

    test

--- learn-c/learn-c/test08.c
+++ learn-c/learn-c/test08.c
@@ -1,27 +1,53 @@
 //#define _CRT_SECURE_NO_WARNINGS 1
 //#include <stdio.h>
 //int main() {
-//	int a = 0;
-//	int b = 0;
-//	int c = 0;
-//	//输入
-//	scanf("%d%d%d", &a, &b, &c);
-//	//调整顺序
-//	if (a < b) {
-//		int tmp = a;
-//		a = b;
-//		b = tmp;
-//	}
-//	if (a < c) {
-//		int tmp = a;
-//		a = c;
-//		c = tmp;
-//	}
-//	if (b < c) {
-//		int tmp = b;
-//		b = c;
-//		c = tmp;
-//	}
-//	//输出
-//	printf("%d %d %d",a,b,c);
+	//int a = 0;
+	//int b = 0;
+	//int c = 0;
+	////输入
+	//scanf("%d%d%d", &a, &b, &c);
+	////调整顺序
+	//if (a < b) {
+	//	int tmp = a;
+	//	a = b;
+	//	b = tmp;
+	//}
+	//if (a < c) {
+	//	int tmp = a;
+	//	a = c;
+	//	c = tmp;
+	//}
+	//if (b < c) {
+	//	int tmp = b;
+	//	b = c;
+	//	c = tmp;
+	//}
+	////输出
+	//printf("%d %d %d",a,b,c);
+
+
+	//求两个数的最大公约数
+	//int m = 0;
+	//int n = 0;
+	//scanf("%d %d", &m, &n);
+	//int max = m>n?n:m;//设max为m和n的最大公约数
+	//while (1) {
+	//	if (m % max == 0 && n % max == 0) {
+	//		printf("最大公约数是：%d\n", max);
+	//		break;
+	//	}
+	//	max--;
+	//}
+
+	//辗转相除法求最大公约数
+	//int m = 0;
+	//int n = 0;
+	//int t = 0;
+	//scanf("%d%d", &m, &n);
+	//while (t = m % n) //无须比较m和n的大小
+	//{
+	//	m = n;
+	//	n = t;
+	//}
+	//printf("最大公约数为：%d", n);
 //}
