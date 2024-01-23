f8a1f628538e10d5cf54918c9e3b9ad4d4e0cb7c
gubei6346 <gubeiiii6346@gmail.com>
2024-01-23 18:31:34 +0800

    test

--- learn-c/learn-c/test010.c
+++ learn-c/learn-c/test010.c
@@ -2,6 +2,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <string.h>
+int get_max(int x, int y) {
+	int z = 0;
+	if (x > y)
+		z = x;
+	else
+		z = y;
+	return z;
+}
+//函数返回类型的地方写void表示这个函数不返回任何值
+void Swap(int *pa, int* pb) { //函数定义     
+	int z = 0;        //形式参数 形参
+	z = *pa;
+	*pa = *pb;
+	*pb = z;
+}
 int main() {
 	//char arr1[20] = { 0 };
 	//char arr2[] = "hello";
@@ -11,4 +26,19 @@ int main() {
 	//char arr[] = "helloooo";
 	//memset(arr, 'x', 5);//将arr的前5个字符转换成x
 	//printf("%s\n", arr);
+
+	//自定义函数
+	//求最大值
+	/*int a = 10;
+	int b = 20;
+	int max = get_max(a,b);
+	printf("max=%d\n", max);*/
+
+	//交换两个数
+	int a = 10;
+	int b = 20;
+	printf("交换前：a=%d b=%d\n", a, b);
+	Swap(&a, &b);//函数调用     实际参数  实参 可以是常量、变量、表达式、函数但必须都是确定的值
+	printf("交换后：a=%d b=%d\n", a, b);
+
 }
