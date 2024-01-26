56e64272402b2530a8004fe61080e64aec5927ba
gubei6346 <gubeiiii6346@gmail.com>
2024-01-26 15:52:56 +0800

    test

--- learn-c/learn-c/test012.c
+++ learn-c/learn-c/test012.c
@@ -1,6 +1,18 @@
-//函数嵌套调用和链式访问
+//函数递归
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+void print(unsigned int n){
+	if (n > 9) {
+		print(n / 10);//限制条件
+	}
+	printf("%d ", n % 10);
+}
 int main() {
+	/*printf("111");
+	main();*/
+
+	unsigned int num = 1234;
+	//scanf("%u ", &num);
+	print(num);//print函数可以打印参数部分数字的每一位
 
 }
