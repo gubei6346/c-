110083562926c079611ee32739da08ca311a3c81
gubei6346 <gubeiiii6346@gmail.com>
2024-01-27 14:27:35 +0800

    test

--- learn-c/learn-c/test012.c
+++ learn-c/learn-c/test012.c
@@ -1,7 +1,7 @@
 //函数递归
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <string.h>
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
 //int my_strlen(char* str) {
 //	int count = 0;
 //	while(*str != '\0') {
@@ -10,24 +10,24 @@
 //	}
 //	return count;
 //}
-int my_strlen(char* str) {//递归
-	if (*str != '\0')
-		return 1 + my_strlen(str + 1);
-	else
-		return 0;
-}
-void print(unsigned int n){
-	if (n > 9) {
-		print(n / 10);//限制条件
-	}
-	printf("%d ", n % 10);
-}
-int Fac(int n) {
-	if (n <= 1)
-		return 1;
-	else
-		return n * Fac(n - 1);
-}
+//int my_strlen(char* str) {//递归
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+//void print(unsigned int n){
+//	if (n > 9) {
+//		print(n / 10);//限制条件
+//	}
+//	printf("%d ", n % 10);
+//}
+//int Fac(int n) {
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac(n - 1);
+//}
 //int Fib(int n) {//效率太低
 //	if (n <= 2)
 //		return 1;
@@ -35,19 +35,19 @@ int Fac(int n) {
 //		return Fib(n - 1) + Fib(n - 2);
 //}
 
-int Fib(int n) {
-	int a = 1;
-	int b = 1;
-	int c = 1;
-	while (n>2) {
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	return c;
-}
-int main() {
+//int Fib(int n) {
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n>2) {
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main() {
 	/*printf("111");
 	main();*/
 
@@ -74,8 +74,8 @@ int main() {
 	int ret=Fac(n);
 	printf("%d\n", ret);*/
 
-	int n = 0;
+	/*int n = 0;
 	scanf("%d", &n);
 	int ret = Fib(n);
 	printf("%d\n", ret);
-}
+}*/
