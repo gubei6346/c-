5300850e19c7bb211e6a71da6ec63383418c6751
gubei6346 <gubeiiii6346@gmail.com>
2024-01-28 18:25:55 +0800

    test

--- learn-c/learn-c/test013.c
+++ learn-c/learn-c/test013.c
@@ -1,6 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <string.h>
+int my_strlen(char* str) {
+	int count = 0;
+	while (*str != '\0') {
+		count++;
+		str++;
+	}
+	return count;
+}
+void reverse_string(char* str) {
+	int left = 0;
+	int right = my_strlen(str) - 1;
+	while (left < right) {
+		char tmp = str[left];
+		str[left] = str[right];
+		str[right] = tmp;
+		left++;
+		right--;
+	}
+}
 int main() {
 	//加减交替求分数的和
 	/*int i = 0;
@@ -22,11 +41,16 @@ int main() {
 	}
 	printf("%d\n", max);*/
 	//乘法口诀表
-	int i = 1;
-	for (i = 1; i <= 9; i++) {
-		for (int j = 1; j <= i; j++) {
-			printf("%d*%d=%2d ", j, i,i*j);//-2d
-		}
-		printf("\n");
-	}
+	//int i = 1;
+	//for (i = 1; i <= 9; i++) {
+	//	for (int j = 1; j <= i; j++) {
+	//		printf("%d*%d=%2d ", j, i,i*j);//-2d
+	//	}
+	//	printf("\n");
+	//}
+
+	//将字符串逆序
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s\n", arr);
 }
