9f03d2600464ae8078cad2a7869a2a202ccd5922
gubei6346 <gubeiiii6346@gmail.com>
2024-01-28 18:42:43 +0800

    test

--- learn-c/learn-c/test013.c
+++ learn-c/learn-c/test013.c
@@ -9,16 +9,28 @@ int my_strlen(char* str) {
 	}
 	return count;
 }
+//void reverse_string(char* str) {
+//	int left = 0;
+//	int right = my_strlen(str) - 1;
+//	while (left < right) {
+//		char tmp = str[left];
+//		str[left] = str[right];
+//		str[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//递归
 void reverse_string(char* str) {
-	int left = 0;
-	int right = my_strlen(str) - 1;
-	while (left < right) {
-		char tmp = str[left];
-		str[left] = str[right];
-		str[right] = tmp;
-		left++;
-		right--;
+	char tmp = *str;
+	int len = my_strlen(str);
+	*str = *(str + len - 1);
+	*(str + len - 1) = '\0';
+	if(my_strlen(str+1)>=2){
+		reverse_string(str + 1);
 	}
+	
+	*(str + len - 1) = tmp;
 }
 int main() {
 	//加减交替求分数的和
